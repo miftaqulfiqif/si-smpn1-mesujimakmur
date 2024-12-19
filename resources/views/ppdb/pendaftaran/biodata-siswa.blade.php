ad0a94a941f162d7e40e99082b08c783550d034f
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2024-12-19 21:33:13 +0700

    repaired data calon siswa

--- resources/views/ppdb/pendaftaran/biodata-siswa.blade.php
+++ resources/views/ppdb/pendaftaran/biodata-siswa.blade.php
@@ -203,13 +203,31 @@
                         @enderror
                     </li>
                     <li class="form-control gap-1">
-                        <label for="foto" class="label font-medium">Foto</label>
-                        <input type="file" id="foto" name="foto" accept="image/*"
-                            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                        @if ($biodata && $biodata->foto)
-                            <p>Foto Saat Ini: <img src="{{ asset('storage/' . $biodata->foto) }}" alt="Foto"
-                                    width="100"></p>
-                        @endif
+                        <label for="foto"
+                            class="mt-10 bg-white text-gray-500 font-semibold text-base rounded max-w-md h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed font-[sans-serif]">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-11 mb-2 fill-gray-500" viewBox="0 0 32 32">
+                                <path
+                                    d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+                                    data-original="#000000" />
+                                <path
+                                    d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+                                    data-original="#000000" />
+                            </svg>
+                            Upload file
+
+                            <input type="file" id='foto' name="foto" class="hidden" accept="image/*" />
+                            <p class="text-xs font-medium text-gray-400 mt-2">PNG, JPG and JPEG are Allowed.</p>
+                        </label>
+                        <div id="imagePreview" class="mt-4">
+                            @if ($biodata && $biodata->foto)
+                                <input type="hidden" name="foto_lama" value="{{ $biodata->foto }}">
+                                <img id="previewImg" src="{{ asset('storage/' . $biodata->foto) }}"
+                                    alt="Preview Foto Lama" width="100" />
+                            @else
+                                <img id="previewImg" src="" alt="Preview Foto Baru" width="100"
+                                    style="display: none;" />
+                            @endif
+                        </div>
                         @error('foto')
                             <span class="text-red-500 text-sm">{{ $message }}</span>
                         @enderror
@@ -254,6 +272,25 @@
             toggleKipSection();
 
             penerimaKip.addEventListener('change', toggleKipSection);
+        });
+
+        document.getElementById('foto').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            const previewImg = document.getElementById('previewImg');
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(e) {
+                    previewImg.src = e.target.result;
+                    previewImg.classList.remove('hidden');
+                };
+
+                reader.readAsDataURL(file);
+            } else {
+                previewImg.src = '#';
+                previewImg.classList.add('hidden');
+            }
         });
     </script>
 @endsection
--- resources/views/ppdb/pendaftaran/upload-document.blade.php
+++ resources/views/ppdb/pendaftaran/upload-document.blade.php
@@ -55,6 +55,32 @@
                 </li>
             </ul>
 
+            <ul id="input-nilai-form" class="flex flex-col w-full gap-4 h-screen overflow-y-auto px-5 pb-40 md:pb-28">
+                <form action="{{ route('save-nilai') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="id_data_calon_siswa" value="{{ $calonSiswa->id }}">
+
+                    <label for="uploadFile1"
+                        class="bg-white text-gray-500 font-semibold text-base rounded max-w-md h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mx-auto font-[sans-serif]">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-11 mb-2 fill-gray-500" viewBox="0 0 32 32">
+                            <path
+                                d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+                                data-original="#000000" />
+                            <path
+                                d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+                                data-original="#000000" />
+                        </svg>
+                        Upload file
+
+                        <input type="file" id='uploadFile1' class="hidden" />
+                        <p class="text-xs font-medium text-gray-400 mt-2">PNG, JPG SVG, WEBP, and GIF are Allowed.</p>
+                    </label>
+                    <li class="flex justify-center">
+                        <button type="submit" id="submit" class="btn px-10 bg-slate-950 text-white">Submit</button>
+                    </li>
+                </form>
+            </ul>
+
         </div>
     </section>
 @endsection
