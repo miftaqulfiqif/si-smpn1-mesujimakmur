b268862eff15a2c8f14191475e98d0b6e366d8b3
Miftaqul Fiqi Firmansyah <miftaqulfiqifirmansyah131@gmail.com>
2025-01-29 20:27:18 +0700

    bug fix pesan kesalahan

--- app/Http/Controllers/SistemInformasiController.php
+++ app/Http/Controllers/SistemInformasiController.php
@@ -7,9 +7,11 @@ use App\Models\Informasi;
 use App\Models\MainInformation;
 use App\Models\Misi;
 use App\Models\Moto;
+use App\Models\PeriodeDaftar;
 use App\Models\Prestasi;
 use App\Models\Visi;
 use App\Models\VisiMisi;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class SistemInformasiController extends Controller
@@ -26,6 +28,18 @@ class SistemInformasiController extends Controller
         $informasis = Informasi::latest()->limit(4)->get();
 
         $mainInformation = MainInformation::latest()->first();
+
+
+        // set periode non aktif
+        $periode = PeriodeDaftar::where('status', 1)->first();
+        $currentDate = Carbon::now()->format('Y-m-d');
+        $periodeEnd = $periode->end_date;
+
+        if ($currentDate > $periodeEnd) {
+            $periode->update([
+                'status' => 0
+            ]);
+        }
 
         return view('home', compact('moto', 'fotoSekolah', 'visi', 'misi', 'prestasis', 'informasis', 'mainInformation'));
     }
